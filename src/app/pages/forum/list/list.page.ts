e39640f0b5dadd5841fa75c606305137a1204d9b
JaeHo Song <thruthesky@gmail.com>
2018-05-28 13:03:21 +0800

    posting

--- src/app/pages/forum/list/list.page.ts
+++ src/app/pages/forum/list/list.page.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 @Component({
     selector: 'app-forum-list-page',
     templateUrl: 'list.page.html'
 })
 export class ForumListPage {
-
+    post_id = '';
+    constructor( activated: ActivatedRoute ) {
+        activated.paramMap.subscribe( params => {
+            this.post_id = params.get('post_id');
+        });
+    }
 }
 
 
