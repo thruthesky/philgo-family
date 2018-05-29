13ee09cb7dff2f10cceee1fe889d67767adbe625
JaeHo Song <thruthesky@gmail.com>
2018-05-29 10:24:51 +0800

    post edit

--- src/app/pages/forum/list/list.page.ts
+++ src/app/pages/forum/list/list.page.ts
@@ -1,15 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { PostListComponent } from '../../../modules/philgo-api/components/forum/post-list/post-list.component';
+import { ApiPostData } from '../../../modules/philgo-api/providers/philgo-api.service';
+interface Show {
+    mode: 'write' | 'list';
+}
 @Component({
     selector: 'app-forum-list-page',
     templateUrl: 'list.page.html'
 })
 export class ForumListPage {
+    @ViewChild('postListComponent') postListComponent: PostListComponent;
     post_id = '';
-    constructor( activated: ActivatedRoute ) {
-        activated.paramMap.subscribe( params => {
+    show: Show;
+    constructor(activated: ActivatedRoute) {
+        activated.paramMap.subscribe(params => {
+            this.init();
             this.post_id = params.get('post_id');
         });
+    }
+    init() {
+        this.show = {
+            mode: 'list'
+        };
+    }
+    onWrite(event: ApiPostData) {
+        this.postListComponent.write(event);
+        this.postListComponent.display = true;
+    }
+    onEdit(event: ApiPostData) {
+        this.postListComponent.edit(event);
+        this.postListComponent.display = true;
     }
 }
 
--- src/app/pages/forum/view/view.page.ts
+++ src/app/pages/forum/view/view.page.ts
@@ -3,7 +3,9 @@ import { ActivatedRoute } from '@angular/router';
 import { PhilGoApiService } from '../../../modules/philgo-api/philgo-api.module';
 import { ApiPostData } from '../../../modules/philgo-api/providers/philgo-api.service';
 import { PostListComponent } from '../../../modules/philgo-api/components/forum/post-list/post-list.component';
-
+interface Show {
+    mode: 'write' | 'view' | 'edit';
+}
 @Component({
     selector: 'app-forum-view-page',
     templateUrl: 'view.page.html'
@@ -12,13 +14,21 @@ export class ForumViewPage implements AfterViewInit {
 
     @ViewChild('postListComponent') postListComponent: PostListComponent;
     post: ApiPostData;
+
+
+    show: Show = <any> {};
+
     constructor(
         public activated: ActivatedRoute,
         public api: PhilGoApiService
     ) {
     }
+    init() {
+        this.show.mode = 'view';
+    }
     ngAfterViewInit() {
         this.activated.paramMap.subscribe(params => {
+            this.init();
             console.log('idx: ', params.get('idx'));
             this.api.getPost(params.get('idx')).subscribe(post => {
                 console.log('post', post);
